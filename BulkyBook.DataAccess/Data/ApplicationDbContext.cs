98b77819053418166826c696adb723a89b666f30
Leon Moore <leon@sharphue.com>
2023-03-12 17:15:47 -0500

    restructure application

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,13 +1,12 @@
-﻿using BulkyBookWeb.Models;
+﻿using BulkyBook.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyBookWeb.Data
+namespace BulkyBook.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
         {
-            
         }
 
         public DbSet<Category> Categories { get; set; }
